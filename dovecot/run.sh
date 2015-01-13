2415b10af713365ca542ecfb19d39da89bb09480
Jason Gegere <jason@hgmail.com>
2015-01-13 16:06:40 -0600

    output vars for testing, setup rsyslog logentries.conf

--- dovecot/run.sh
+++ dovecot/run.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+
+StartPostfix ()
+{
+    echo "=> Adding the following credentials $USER:$PASS"
+    echo "=> Log Key: $LOG_TOKEN"
+    #$allow_networks = "";
+}
+
+
+# output logs to logentries.com
+cat <<EOF > /etc/rsyslog.d/logentries.conf
+\$template Logentries,"$LOG_TOKEN %HOSTNAME% %syslogtag%%msg%\n"
+
+*.* @@api.logentries.com:10000;Logentries
+EOF
+
+
 # configure things
 /configure.sh
 
 # start necessary services for operation (dovecot -F starts dovecot in the foreground to prevent container exit)
 chown -R vmail:vmail /srv/vmail
 
+# Display Postfix credentials for build testing
+#
+StartPostfix
 
+
+# Spin everything up
+#
 /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
